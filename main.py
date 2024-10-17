91c46f1fb20e2c35ebed9f0f29a2b1c092415408
viviantran706 <79215770+viviantran706@users.noreply.github.com>
2024-10-16 21:09:22 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ led_pins = [
     board.IO21,
     board.IO26, # type: ignore
     board.IO47,
+    board.IO33, # type: ignore
+    board.IO34, # type: ignore
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39,
     # do the rest...
 ]
 
@@ -21,16 +29,39 @@ leds = [DigitalInOut(pin) for pin in led_pins]
 for led in leds:
     led.direction = Direction.OUTPUT
 
+decay_rate = .5
+max_volume = 70000
+sensitvity = .05
+volume_at = 0
+
+def get_volume():
+    raw_value = microphone.value
+    return volume
+
+def update_leds(volume):
+    led_count = len(leds)
+    led_on = int( volume * led_count)
+    for i in range(led_count):
+        leds[i].value = i < led_on
+
+
 # main loop
 while True:
-    volume = microphone.value
-
+    volume = get_volume()
     print(volume)
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
+    if volume > volume_at:
+        display_volume = volume 
+    else:
+        volume_at = max(0,volume_at - decay_rate)
+    update_leds(volume_at)
 
-    sleep(1)
+    print(volume) 
+    sleep(0.05)
+
+
+    
+     
 
     # instead of blinking,
     # how can you make the LEDs
